d9862291928538d465e8942da4a32696a96fdc86
ChunyuBai <baichunyu26@gmail.com>
2023-01-23 18:22:09 -0500

    create some fake info for employee table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "big", last_name: "bob", hourly_rate: 70)
+
+@store1.employees.create(first_name: "Chunyu", last_name: "Bai", hourly_rate: 100)
+
+@store2.employees.create(first_name: "big", last_name: "Sam", hourly_rate: 60)
+
+@store2.employees.create(first_name: "big", last_name: "Boss", hourly_rate: 90)
